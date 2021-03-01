0ce15abadd0d68bf344bcc71785d5acf539ce7fb
edgarable <edgar@able.co>
2021-03-01 15:57:35 -0500

    Create game using TDD

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,55 @@
 class Hangman
+    attr_accessor :word_teaser, :chances
 
+    def initialize(word, clue)
+        @word = word
+        @clue = clue
+        @chances = 7
+        @word_teaser = ""
+    
+        @word.size.times do
+          @word_teaser += "_ "
+        end
+        @word_teaser.strip!
+    end
+
+    def start_game
+        initial_message = "Clue: #{@clue}, and has #{@word.size} letters: #{@word_teaser}"
+    end
+    
+    def make_guess (guess)
+        if @word.include?(guess)
+            good_guess(guess)
+        else
+            bad_guess(guess)
+        end
+    end
+
+    def good_guess(guess)
+        update_teaser(guess)
+
+        return "Congratulations! You guessed the word: #{@word.capitalize}" unless @word_teaser.include?("_")
+
+        "Success! You have #{@chances} chances left: #{@word_teaser}"
+    end
+
+    def bad_guess(guess)
+        @chances -= 1
+
+        return "Game Over" if @chances == 0
+
+        "Try again! You have #{@chances} chances left: #{@word_teaser}"
+    end
+
+    def update_teaser(guess)
+        new_teaser = @word_teaser.split
+    
+        new_teaser.each_with_index do |letter, index|
+          if letter == '_' && @word[index] == guess
+            new_teaser[index] = guess
+          end
+        end
+    
+        @word_teaser = new_teaser.join(' ')
+    end
 end
--- hangman_spec.rb
+++ hangman_spec.rb
@@ -3,8 +3,31 @@ require_relative './hangman'
 
 class HangmanTest < Minitest::Test
 
-    def your_first_test_here
-        
+    def test_initial_message
+        hangman_game = Hangman.new("spain", "Country in Europe")
+        assert_equal "Clue: Country in Europe, and has 5 letters: _ _ _ _ _", hangman_game.start_game
+    end
+
+    def test_make_a_correct_guess
+        hangman_game = Hangman.new("spain", "Country in Europe")
+        assert_equal "Success! You have 7 chances left: _ _ a _ _", hangman_game.make_guess("a")
+    end
+
+    def test_make_a_wrong_guess
+        hangman_game = Hangman.new("spain", "Country in Europe")
+        assert_equal "Try again! You have 6 chances left: _ _ _ _ _", hangman_game.make_guess("x")
+    end
+
+    def test_win_game
+        hangman_game = Hangman.new("spain", "Country in Europe")
+        hangman_game.word_teaser = "_ p a i n"
+        assert_equal "Congratulations! You guessed the word: Spain", hangman_game.make_guess("s")
+    end
+
+    def test_game_over
+        hangman_game = Hangman.new("spain", "Country in Europe")
+        hangman_game.chances = 1
+        assert_equal "Game Over", hangman_game.make_guess("x")
     end
 
 end
